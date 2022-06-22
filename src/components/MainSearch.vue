c8fd9f3e34e71c86ae995a5ac8480eb6783a0dac
sczhou <sczhou@robvision.com>
2022-06-22 18:34:29 +0800

    feat 添加搜索组件(备份提交)

---
+++ src/components/MainSearch.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+        const msg = ref("hello")
+    
+        const song_infos = ref({})
+
+        function log(){
+            console.log(msg.value)
+        }
+
+        function search() {
+            song_infos.value = {id:1, name:"alex"}
+        }
+
+        return {msg,song_infos, search}
+    },
+})
+
+
+
+</script>
+
+<template>
+    <input type="text" v-model="msg" />
+
+    <button v-on:click="search">search</button>
+    <div>
+        <p v-for="(key,item) in song_infos" :key = "item">{{key}} {{item}} </p>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
