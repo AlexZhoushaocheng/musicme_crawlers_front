9c120a39d6cb06d62b5fd5073a480bd90dc4d99e
Alex Zhou <alex@robversion.com>
2022-06-23 08:30:56 +0800

    feat show search result

--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -1,35 +1,50 @@
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent } from 'vue'
 import { ref } from 'vue'
+import axios from 'axios'
 
-export default defineComponent({
-    setup() {
-        const msg = ref("hello")
-    
-        const song_infos = ref({})
+const search_txt = ref("")
+const song_infos = ref({})
 
-        function log(){
-            console.log(msg.value)
+function search(keyword: MouseEvent) {
+  // console.log(search_input.value)
+
+  // axios.get('/search/等一分钟').then((response) => {
+  //   console.log(response.data)
+  // })
+ 
+  axios({
+    // headers: {"Access-Control-Allow-Origin": "*"} ,
+    method: 'get',
+    url: '/api/search/' + search_txt.value,
+    // timeout: 1000,
+    responseType: 'json'
+  }).then((response) => {
+    console.log('hello world')
+    console.log(response.data)
+    console.log(typeof response.data)
+    console.log(response.status)
+
+    if(response.status == 200){
+        try{
+            song_infos.value = response.data['result']['songs']
         }
+        catch(ex){
 
-        function search() {
-            song_infos.value = {id:1, name:"alex"}
         }
-
-        return {msg,song_infos, search}
-    },
-})
-
-
+    }
+  })
+}
 
 </script>
 
 <template>
-    <input type="text" v-model="msg" />
+    <input type="text" v-model="search_txt" />
 
     <button v-on:click="search">search</button>
     <div>
-        <p v-for="(key,item) in song_infos" :key = "item">{{key}} {{item}} </p>
+        <div v-for="(key,item) in song_infos" :key = "item">{{key['name']}} {{key['ar'][0]['name']}} <button>下载</button></div>
+        
     </div>
 
 </template>
